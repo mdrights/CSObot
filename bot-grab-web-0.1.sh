b8e120212e78c57ee1bfe1c9f0c596c6b2ae1f6a
mdrights <linusyeung@live.com>
2016-07-26 05:56:23 +0200

    update bot0.1

--- bot-grab-web-0.1.sh
+++ bot-grab-web-0.1.sh
@@ -3,20 +3,19 @@
 # 2016.07.23
 
 
-Text="$HOME/Public/bot-website-links.html"
-MDText="$HOME/Public/bot-md-links.txt"
+Text="$HOME/bot-website-links.html"
+MDText="$HOME/bot-md-links.txt"
 Token="260947680:AAF87IQ2967PLVOhVWdU2xlGZnHz5_gq49o"
 Chatid="64960773"
-Date="2016-07-22"  # date +%Y-%m-%d
+Date="2016-07-25"  # date +%Y-%m-%d
 
-echo "News from NGOCN.net" > $Text
+echo "News from NGOCN.net on $Date." > $Text
 curl http://www.ngocn.net | grep "www.ngocn.net/news/$Date" >> $Text
 
-cd $HOME/Public/
 
-pandoc -f html -t markdown bot-website-links.html -o $MDText
+pandoc -f html -t markdown $Text -o $MDText
 
-proxychains4 w3m "https://api.telegram.org/bot$Token/sendmessage?chat_id=$Chatid&text=`cat $MDText`" 1&>/dev/null
+w3m "https://api.telegram.org/bot$Token/sendmessage?chat_id=$Chatid&text=`cat $MDText`" 1&>/dev/null
 
 echo "News had been sent."
 exit 0
