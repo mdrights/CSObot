b33a5c8aa1791a32befdfde1ed99274fe77bdab6
mdrights <linusyeung@live.com>
2016-07-29 18:05:10 +0200

    refine bot-npc

--- bot-npc.sh
+++ bot-npc.sh
@@ -14,16 +14,16 @@ Date="`date +%Y-%m-%d`"
 
 #----------------------------
 
-echo "Today's news from 人大常委会法律草案征求意见" > $Text
+echo "人大常委会法律草案征求意见" > $Text
 echo "" >> $Text
 
 curl http://www.npc.gov.cn/npc/flcazqyj/node_8176.htm | grep "<a href=\"\.\.\/\.\.\/COB" >> $Text
 
 pandoc -f html -t markdown $Text -o $MDText
 
-sed '1s/^/正在进行征求意见\n/' $Text
-
 sed 's/\.\.\/\.\./http:\/\/www.npc.gov.cn/g' $MDText > $MDText1
+
+# sed '2s/^/正在征求意见：/g
 
 w3m "https://api.telegram.org/bot$Token/sendmessage?chat_id=$Chatid&text=`cat $MDText1`" 1&>/dev/null
 
