e57924fd807abb500fa5b04d1257491a0c17dc31
mdrights <linusyeung@live.com>
2016-07-31 16:24:55 +0200

    make refinement and deploy.

--- bot-update-reply-vps.sh
+++ bot-update-reply-vps.sh
@@ -4,20 +4,20 @@
 # 1.0.0 alpha
 
 # Set file receiving original updates. 
-Update="$HOME/updates1.json"
-Update1="$HOME/updates1.txt"
-NewMsg=$HOME/new-message.txt
-IdList="$HOME/id-list.txt"
-Cmd_Rp="$HOME/commands-reply.txt"
-MsgIdNew=$HOME/message-id-new.txt
-MsgIdOld=$HOME/message-id-old.txt
+Update="$HOME/github/CSObot/updates1.json"
+Update1="$HOME/github/CSObot/updates1.txt"
+NewMsg=$HOME/github/CSObot/new-message.txt
+IdList="$HOME/github/CSObot/id-list.txt"
+Cmd_Rp="$HOME/github/CSObot/commands-reply.txt"
+MsgIdNew=$HOME/github/CSObot/message-id-new.txt
+MsgIdOld=$HOME/github/CSObot/message-id-old.txt
 
 
 if [ ! -x $Idlist ];then
 	touch $Idlist
 fi
 
-curl --socks5-hostname 127.0.0.1:1080 https://api.telegram.org/bot260947680:AAF87IQ2967PLVOhVWdU2xlGZnHz5_gq49o/getupdates > $Update
+curl https://api.telegram.org/bot260947680:AAF87IQ2967PLVOhVWdU2xlGZnHz5_gq49o/getupdates > $Update
 
 
 # Function1: Grab old chatid's message information (id & text), and send reply back.
@@ -36,7 +36,7 @@ send_msg () {
 
 	Rpl="`grep "$Cmd_rv" $Cmd_Rp | awk -F : '// { print $2 ; }'`"     # Grab the correct answer.
 									# Send it!
-	proxychains4 w3m "https://api.telegram.org/bot260947680:AAF87IQ2967PLVOhVWdU2xlGZnHz5_gq49o/sendmessage?chat_id=$1&text=$Rpl&parse_mode=Markdown" 1&>/dev/null
+	w3m "https://api.telegram.org/bot260947680:AAF87IQ2967PLVOhVWdU2xlGZnHz5_gq49o/sendmessage?chat_id=$1&text=$Rpl&parse_mode=Markdown" 1&>/dev/null
 	echo "$Rpl"
 }
 
@@ -65,7 +65,7 @@ grab_msg () {
 	else
 		echo "This is an new id !"
 		echo "$MsgId" >> $IdList    # Put the new id into the list.
-		proxychains4 w3m "https://api.telegram.org/bot260947680:AAF87IQ2967PLVOhVWdU2xlGZnHz5_gq49o/sendmessage?chat_id=$MsgId&parse_mode=Markdown&text=Hi,there! 很高兴带上我～" 1&>/dev/null
+		w3m "https://api.telegram.org/bot260947680:AAF87IQ2967PLVOhVWdU2xlGZnHz5_gq49o/sendmessage?chat_id=$MsgId&parse_mode=Markdown&text=Hi,there! 很高兴带上我～" 1&>/dev/null
 	fi
 }
 
