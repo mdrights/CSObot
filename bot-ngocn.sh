2ba6bf1bafd4e2f31ffd9de87e9f951f0ad2d5a5
mdrights <linusyangcn@gmail.com>
2016-08-02 04:45:00 +0000

    try1 to add empty-content-optout notification, on ngocn script.

--- bot-ngocn.sh
+++ bot-ngocn.sh
@@ -10,18 +10,25 @@ Token="260947680:AAF87IQ2967PLVOhVWdU2xlGZnHz5_gq49o"
 Chatid="`cat $HOME/github/CSObot/id-list.txt`"
 Date="`date +%Y-%m-%d`"
 
-echo "<em>Today's news from NGOCN.net, $Date.</em>" > $Text
-echo >> $Text
 
-curl http://www.ngocn.net | grep "www.ngocn.net/news/$Date" | grep -v "/upload/" >> $Text
+curl http://www.ngocn.net | grep "www.ngocn.net/news/$Date" | grep -v "/upload/" > $Text
 
+if [ -s $Text ]; then
+	echo "<em>Today's news from NGOCN.net, $Date.</em>" >> $Text
+	pandoc -f html -t markdown $Text -o $MDText
 
-pandoc -f html -t markdown $Text -o $MDText
-
-for i in $Chatid;
-do
-w3m "https://api.telegram.org/bot$Token/sendmessage?chat_id=$i&text=`cat $MDText`&parse_mode=Markdown" 1&>/dev/null
-done
+	for i in $Chatid;
+	do
+	w3m "https://api.telegram.org/bot$Token/sendmessage?chat_id=$i&text=`cat $MDText`&parse_mode=Markdown" 1&>/dev/null
+	done
+else
+	echo "*Today's news from NGOCN.net, $Date.* \nOops，现在还没有新的内容！" >> $Text
+	
+	for i in $Chatid;
+	do
+	w3m "https://api.telegram.org/bot$Token/sendmessage?chat_id=$i&text=`cat $Text`&parse_mode=Markdown" 1&>/dev/null
+	done
+fi
 
 echo "News had been sent."
 exit 0
