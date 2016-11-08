400e767d862d3f359027bda13b47c3d4fa897443
mdrights <linusyangcn@gmail.com>
2016-11-08 13:47:11 +0000

    retry #2

--- bot-gwy-bw.sh
+++ bot-gwy-bw.sh
@@ -23,7 +23,7 @@ curl http://www.moe.gov.cn/s78/A02/zfs__left/s5911/moe_621/ | grep "href=.*$Mont
 curl http://www.moe.gov.cn/jyb_xwfb/s248/ | grep "href=.*$Month" | sed 's/=\"\./=\"http:\/\/www.moe.gov.cn\/jyb_xwfb\/s248/g' >> $Text 
 	# 徵求意見
 
-        pandoc -f html -t markdown_github $Text | sed '1s/^/**教育政策和教育部門規章**     /g' | sed 's/ \".*\"//g' > $MDText
+        pandoc -f html -t markdown $Text | sed '1s/^/**教育政策和教育部門規章**     /g' | sed 's/ \".*\"//g' > $MDText
 
 . $HOME/CSObot/toMe.sh "$Text" "$MDText" "教育部"
 
@@ -31,7 +31,7 @@ curl http://www.moe.gov.cn/jyb_xwfb/s248/ | grep "href=.*$Month" | sed 's/=\"\./
 
 curl http://www.nhfpc.gov.cn/zhuzhan/gongw/lists.shtml | grep "href=.*$Month" | sed 's/\.\.\/\.\./http:\/\/www.nhfpc.gov.cn/g' > $Text
 
-        pandoc -f html -t markdown_github $Text | sed '1s/^/**衛生計生委文件發佈**     /g' | sed 's/ \".*\"//g' > $MDText
+        pandoc -f html -t markdown $Text | sed '1s/^/**衛生計生委文件發佈**     /g' | sed 's/ \".*\"//g' > $MDText
 
 . $HOME/CSObot/toMe.sh "$Text" "$MDText" "卫生计生委"
 
@@ -39,14 +39,14 @@ curl http://www.nhfpc.gov.cn/zhuzhan/gongw/lists.shtml | grep "href=.*$Month" | 
 
 curl http://www.mohrss.gov.cn/SYrlzyhshbzb/zcfg/ | grep "href=.*$Month" | sed 's/\.\//http:\/\/www.mohrss.gov.cn\/SYrlzyhshbzb\/zcfg\//g' > $Text
 
-        pandoc -f html -t markdown_github $Text | sed '1s/^/**人社部文件發佈**     /g' | sed 's/ \".*\"//g' > $MDText
+        pandoc -f html -t markdown $Text | sed '1s/^/**人社部文件發佈**     /g' | sed 's/ \".*\"//g' > $MDText
 
 . $HOME/CSObot/toMe.sh "$Text" "$MDText" "人社部"
 
 # 5.------------------------
 curl http://www.mohurd.gov.cn/wjfb/index.html | grep "href=.*$Month" > $Text
 
-        pandoc -f html -t markdown_github $Text | sed '1s/^/**住建部文件發佈**     /g' > $MDText
+        pandoc -f html -t markdown $Text | sed '1s/^/**住建部文件發佈**     /g' > $MDText
 
 . $HOME/CSObot/toMe.sh "$Text" "$MDText" "住建部"
 
@@ -58,7 +58,7 @@ curl http://www.miit.gov.cn/n1146295/n1146557/n1146624/index.html | grep -2 "$Mo
 curl http://www.miit.gov.cn/n1146295/n1146557/index.html | grep -2 "$Month1" | sed 's/\.\.\/\.\./http:\/\/www.miit.gov.cn/g' >> $Text
 	# 法律法規
 
-        pandoc -f html -t markdown_github $Text | sed '1s/^/**工信部文件和規章發佈**     /g' > $MDText
+        pandoc -f html -t markdown $Text | sed '1s/^/**工信部文件和規章發佈**     /g' > $MDText
 
 . $HOME/CSObot/toMe.sh "$Text" "$MDText" "工信部"
 
--- toMe.sh
+++ toMe.sh
@@ -3,7 +3,7 @@
 Token=260947680:AAF87IQ2967PLVOhVWdU2xlGZnHz5_gq49o
 
 if [ -s "$1" ]; then
-	wget -O - "https://api.telegram.org/bot$Token/sendmessage?chat_id=64960773&parse_mode=Markdown&text=$(cat $2)" > /dev/null 
+	wget -O - "https://api.telegram.org/bot$Token/sendmessage?chat_id=64960773&parse_mode=Markdown&text=$(cat $2)" > /dev/null
 else
         wget -O - "https://api.telegram.org/bot$Token/sendmessage?chat_id=64960773&parse_mode=Markdown&text=Oops, no news from $3." > /dev/null 
 fi
