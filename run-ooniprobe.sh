241d0ea13411ba8c1d3c51a125710566af3b60b3
mdrights <mdrights@tutanota.de>
2019-11-15 02:24:20 +0000

    run-ooniprobe.sh: refined.

--- run-ooniprobe.sh
+++ run-ooniprobe.sh
@@ -3,15 +3,27 @@
 # Changelog
 # 2019-06-09 created
 # 2019-07-06 add a func to parse the result.
+# 2019-11-12 add FnGenNewsSites()
 
+PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:$PATH
 LOG_FILE="/tmp/run-ooniprobe.log"
 TEST_FILE="$HOME/.ooni/inputs/citizenlab-cn-list"
 SELF_PATH=$(dirname $0)
-JQ=$(which jq 2>/dev/null)
-OONI=$(which ooniprobe 2>/dev/null)
+JQ=$(/usr/bin/which jq 2>/dev/null)
+OONI=$(/usr/bin/which ooniprobe 2>/dev/null)
+#OONI="/usr/local/bin/ooniprobe"
+REPO="$HOME/repo/test-lists"
+RES_FILE="$HOME/.ooni/inputs/news-sites-zh.list"
 
 [[ -z $JQ ]] && echo "I depend on jq but it is not installed? Quit."
 [[ -z $OONI ]] && echo "I depend on ooniprobe but it is not installed? Quit."
+
+
+FnGenNewsSites()
+{
+	cat $REPO/lists/cn.csv $REPO/lists/hk.csv $REPO/lists/tw.csv |grep NEWS |awk -F',' '{ print $1 }' |awk -F'/' '{ print $1$2"//"$3 }' |sort |uniq > $RES_FILE 
+
+}
 
 FnParseResult()
 {
@@ -19,11 +31,12 @@ FnParseResult()
 	ANOM=$($JQ '.results' $RET_DIR/summary.json |grep 'anomaly.*true' |wc -l)
 
 	echo "Result: $ANOM of $TOTAL websites are anomaly." |tee -a $LOG_FILE
-
 }
 
 
-FnTestCN()
+# $1: Test File
+# $2: Name of the test
+FnRunTest()
 {
 	ret=0
 	DATE=$(date +%Y%m%dT%H%M)
@@ -31,12 +44,13 @@ FnTestCN()
 	## Testing global test lists.
 	echo "==== $DATE ==== " |tee $LOG_FILE
 	cd
-	$OONI -v blocking/web_connectivity -t 120 -f $TEST_FILE
+	  $OONI  blocking/web_connectivity -t 120 -f $1 > /tmp/ooniprobe-result.log 
 
+	[[ $? -ne 0 ]] && echo "Oops, running OONI probe failed..." |tee -a $LOG_FILE
 	#/usr/local/bin/ooniprobe -v blocking/web_connectivity -d 91.239.100.100 -t 120 -f ./.ooni/inputs/data/my-lists_cn.txt
 	#/usr/local/bin/ooniprobe -v blocking/web_connectivity -u https://tails.boum.org
 
-	echo "==== Finished testing for CN list ====" |tee -a $LOG_FILE
+	echo "==== Test Result for list: $2 ====" |tee -a $LOG_FILE
 
 	# Find the latest measurement (result).
 	RET_DIR=$(find $HOME/.ooni/measurements/ -maxdepth 1 -type d |sort -r |head -n1)
@@ -51,6 +65,12 @@ FnTestCN()
 	fi
 
 	cd -
+		
+}
+
+
+FnSendResult()
+{
 	# Send out the data
 	#/usr/bin/torsocks python2 $SELF_PATH/irc-send-oftc.py $LOG_FILE
 	python2 $SELF_PATH/irc-send-oftc.py $LOG_FILE
@@ -61,12 +81,14 @@ FnTestCN()
 		echo "==== The data has NOT been sent. ====" |tee -a $LOG_FILE
 		ret=1
 	fi
-		
+
 }
 
 
 ## Run them.
-FnTestCN
+FnGenNewsSites
+FnRunTest $RES_FILE "Chinese-lang News Agencies"
+FnSendResult
 
-echo -e "\n>>>> Done. <<<<" |tee -a $LOG_FILE
+#echo -e "\n>>>> Done. <<<<" |tee -a $LOG_FILE
 [[ $ret -eq 0 ]] && exit 0 || exit 1
