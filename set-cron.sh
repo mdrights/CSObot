1d56c1ca7024fbb51ab0e92d0fb4954eca52ea8b
mdrights <mdrights@tutanota.de>
2019-11-15 03:48:52 +0000

    set-cron.sh: added

---
+++ set-cron.sh
@@ -0,0 +1,6 @@
+echo "
+0 0 * * * bash /home/user/repo/csobot/run-ooniprobe.sh
+0 23 * * * bash /home/user/repo/csobot/check-ipfs-gateway.sh
+0 */12 * * * bash /home/user/repo/csobot/check-signalapp.sh
+"  
+
