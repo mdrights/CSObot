af2d3afd8f3330d2111e65e45146aa98f52a0aa8
mdrights <mdrights@tutanota.de>
2021-11-13 10:14:04 +0000

    add check-my-website.sh

---
+++ check-my-website.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#PURPOSE	Check my website availability and send the results to IRC/Matrix.
+#AUTHOR		MDrights
+
+SELF_PATH=$(dirname $0)
+SITE="$1"
+PROXY="env https_proxy=http://192.168.1.173:8118"
+LOG_FILE="/tmp/check-website.log"
+
+echo "
+===================================
+>> Today's Results of Site01:"  # |tee -a ${LOG_FILE}
+
+${PROXY} curl -sS -I ${SITE} | head -4 |tee -a ${LOG_FILE}
+
+HOUR=$(date '+%H')
+
+if [ "$HOUR" -eq 10 ]; then
+	echo ">> It is time to send the result."
+	${PROXY} python2.7 $SELF_PATH/irc-send-oftc.py $LOG_FILE
+	# Clear the result log
+	[ $? -eq 0 ] && echo > $LOG_FILE
+fi
